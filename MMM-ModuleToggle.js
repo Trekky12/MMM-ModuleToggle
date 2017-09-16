2d4c3bef9f2d1eb662e3fd516fefecb6cfd8bfee
Tobias <thaegi@gmx.de>
2017-09-16 16:06:39 +0200

    fix lockString

--- MMM-ModuleToggle.js
+++ MMM-ModuleToggle.js
@@ -6,29 +6,30 @@ Module.register("MMM-ModuleToggle",{
         speed: 1000
     },
     notificationReceived: function(notification, payload, sender){
+        var self = this;
         
         if (notification === 'DOM_OBJECTS_CREATED'){
-            var hideOnStart = MM.getModules().withClass(this.config.hide);
+            var hideOnStart = MM.getModules().withClass(self.config.hide);
             
             hideOnStart.enumerate(function(module) {
                 var callback = function(){};
-                var options = {lockString: this.identifier};
-                module.hide(this.config.speed, callback, options);
+                var options = {lockString: self.identifier};
+                module.hide(self.config.speed, callback, options);
             });
         }
         
 
         if (notification === "MODULE_TOGGLE") {
             
-            Log.log(this.name + " received a module notification: " + notification + " from sender: " + sender.name);
+            Log.log(self.name + " received a module notification: " + notification + " from sender: " + sender.name);
             
             var hide = MM.getModules().withClass(payload.hide);
             
             hide.enumerate(function(module) {
                 Log.log("Hide "+ module.name);
                 var callback = function(){};
-                var options = {lockString: this.identifier};
-                module.hide(this.config.speed, callback, options);
+                var options = {lockString: self.identifier};
+                module.hide(self.config.speed, callback, options);
             });
             
             var show = MM.getModules().withClass(payload.show);
@@ -36,19 +37,19 @@ Module.register("MMM-ModuleToggle",{
             show.enumerate(function(module) {
                 Log.log("Show "+ module.name);
                 var callback = function(){};
-                var options = {lockString: this.identifier};
-                module.show(this.config.speed, callback, options);
+                var options = {lockString: self.identifier};
+                module.show(self.config.speed, callback, options);
             });
             
             var toggle = MM.getModules().withClass(payload.toggle);
             
             toggle.enumerate(function(module) {
                 var callback = function(){};
-                var options = {lockString: this.identifier};
+                var options = {lockString: self.identifier};
                 if(module.hidden){
-                    module.show(this.config.speed, callback, options);
+                    module.show(self.config.speed, callback, options);
                 }else{
-                    module.hide(this.config.speed, callback, options);
+                    module.hide(self.config.speed, callback, options);
                 }
             });
             
