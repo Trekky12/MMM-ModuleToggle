c9cc1ea66a03b9e365ab7a0fec9d8371031f58e2
Entropy159 <115195040+Entropy159@users.noreply.github.com>
2023-10-10 11:06:18 -0600

    Update MMM-ModuleToggle.js

--- MMM-ModuleToggle.js
+++ MMM-ModuleToggle.js
@@ -40,7 +40,7 @@ Module.register("MMM-ModuleToggle",{
             var callback = function(){};
             var options = {lockString: self.identifier};
             
-            console.log(module.name);
+            console.log(module.id);
 
 //            if (true) { // modulesList.includes(module.name)
 //                switch(type){
